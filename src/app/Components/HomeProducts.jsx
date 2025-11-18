d5ca4d66da5bfd7802e8cfa9f90ce66862db3ed9
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-18 17:43:00 +0600

    some fixes

--- src/app/Components/HomeProducts.jsx
+++ src/app/Components/HomeProducts.jsx
@@ -1,46 +1,36 @@
-
-import { Tabs } from 'antd';
-import ProductCard from "./common/ProductCard";
-import NewArrival from './NewArrival';
-import BestSellers from './BestSellers';
-import Featured from './Featured';
+import { Tabs } from "antd";
+import NewArrival from "./NewArrival";
+import BestSellers from "./BestSellers";
+import Featured from "./Featured";
 
 const HomeProducts = () => {
-
-
-
-
-
-const items = [
-  {
-    key: '1',
-    label: 'New Arrival',
-    children: <NewArrival/>
-  },
-  {
-    key: '2',
-    label: 'Bestseller',
-    children: <BestSellers/>,
-  },
-  {
-    key: '3',
-    label: 'Featured Products',
-    children: <Featured/>,
-  },
-];
-
+  const items = [
+    {
+      key: "1",
+      label: "New Arrival",
+      children: <NewArrival />,
+    },
+    {
+      key: "2",
+      label: "Bestseller",
+      children: <BestSellers />,
+    },
+    {
+      key: "3",
+      label: "Featured Products",
+      children: <Featured />,
+    },
+  ];
 
   return (
     <>
-   
-      <div className="container">
-        <div className="text-[18px] text-secText font-medium flex items-center gap-8">
-  
+      <section className="px-4 lg:px-0">
+        <div className="container">
+          <div className="text-[18px] text-secText font-medium flex items-center active:text-black gap-4 lg:gap-8">
+            <Tabs defaultActiveKey="1" items={items} tabBarGutter={32} />
+          </div>
         </div>
-        <div className="mt-8 ">
-             <Tabs defaultActiveKey="1" items={items}  />
-        </div>
-      </div>
+      </section>
     </>
   );
 };
--- src/app/Components/common/ProductCard.jsx
+++ src/app/Components/common/ProductCard.jsx
@@ -4,21 +4,21 @@ import React from "react";
 const ProductCard = ({ img, titel, price }) => {
   return (
     <>
-      <div className="w-[268px] h-[432px] bg-[#F6F6F6] flex flex-col items-center justify-center rounded-[9px]">
-        <div className="img">
+      <div className="w-[163px] lg:w-[268px] h-[352px] lg:h-[432px] bg-[#F6F6F6] flex flex-col items-center justify-center rounded-[9px]">
+        <div className="img max-w-26 lg:max-w-40">
 
-        <Image width={160} height={160} src={img} />
+        <Image width={160} height={160} src={img} alt={titel} />
         </div>
         <div className="det mt-4 mb-6">
           <h3 className="text-base text-black font-medium text-center">
             {titel}
           </h3>
           <h2 className="text-2xl text-black font-semibold text-center mt-4">
-            {price}
+            ${price}
           </h2>
         </div>
 
-        <button className="w-[188px] h-[48px] rounded-[8px] bg-black text-sm text-white font-medium flex items-center justify-center">
+        <button className="w-[139px] lg:w-[188px] h-[48px] rounded-[8px] bg-black text-sm text-white font-medium flex items-center justify-center">
           Buy Now
         </button>
       </div>
